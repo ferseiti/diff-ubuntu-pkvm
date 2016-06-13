cb7370fff75321611dc32f5eb37154c7b274f6e4
Fernando Seiti Furusato <ferseiti@br.ibm.com>
2016-06-13 19:13:22 -0300

    Using mktemp to create and delete files on the run
    Signed-off-by: Fernando Seiti Furusato <ferseiti@br.ibm.com>

--- diff-ubuntu-pkvm.sh
+++ diff-ubuntu-pkvm.sh
@@ -8,6 +8,10 @@ GIT_DIR=$1
 BRANCH1=$2
 BRANCH2=$3
 CUR_DIR=`pwd`
+COMMIT_BRANCH1=`mktemp commit.XXX`
+COMMIT_BRANCH2=`mktemp commit.XXX`
+BRANCH_LOG1=`mktemp branch_log.XXX`
+BRANCH_LOG2=`mktemp branch_log.XXX`
 
 function usage() 
 {
@@ -22,11 +26,20 @@ function get_git_log()
 {
 	cd $GIT_DIR
 	git checkout $BRANCH1
-	git log e2f712dc927e3b9a981ecd86a64d944d0b140322..HEAD --pretty=oneline > $CUR_DIR/$BRANCH1.log
+	git log e2f712dc927e3b9a981ecd86a64d944d0b140322..HEAD --pretty=oneline > $CUR_DIR/$BRANCH_LOG1
 	git branch
+
 	git checkout $BRANCH2
-	git log e2f712dc927e3b9a981ecd86a64d944d0b140322..HEAD --pretty=oneline > $CUR_DIR/$BRANCH2.log
+	git log e2f712dc927e3b9a981ecd86a64d944d0b140322..HEAD --pretty=oneline > $CUR_DIR/$BRANCH_LOG2
 	git branch
+}
+
+function search_commit()
+{
+	cut -f1 -d' ' $CUR_DIR/$BRANCH_LOG2 > $CUR_DIR/$COMMIT_BRANCH2
+#	while read commit; do
+#		fgrep "$commit" $CUR_DIR/$BRANCH1.log
+#	done< "$CUR_DIR/commit_$BRANCH1.log"
 }
 
 if [ ! -d $1 ]; then
@@ -41,3 +54,6 @@ else
 fi
 
 get_git_log
+search_commit
+cd $CUR_DIR
+#rm $COMMIT_BRANCH1 $COMMIT_BRANCH2 $BRANCH_LOG1 $BRANCH_LOG2
