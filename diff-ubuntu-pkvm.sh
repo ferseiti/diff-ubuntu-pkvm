0d2ff0a8120c47e3517fb2e3ac03049004a8140d
Fernando Seiti Furusato <ferseiti@br.ibm.com>
2016-06-13 21:31:38 -0300

    Effectively search commits and output to a file
    Signed-off-by: Fernando Seiti Furusato <ferseiti@br.ibm.com>

--- diff-ubuntu-pkvm.sh
+++ diff-ubuntu-pkvm.sh
@@ -8,10 +8,13 @@ GIT_DIR=$1
 BRANCH1=$2
 BRANCH2=$3
 CUR_DIR=`pwd`
-COMMIT_BRANCH1=`mktemp commit.XXX`
-COMMIT_BRANCH2=`mktemp commit.XXX`
-BRANCH_LOG1=`mktemp branch_log.XXX`
-BRANCH_LOG2=`mktemp branch_log.XXX`
+
+# A lot of temorary files which will be deleted
+COMMIT_BRANCH1=$CUR_DIR/`mktemp commit.XXX`
+COMMIT_BRANCH2=$CUR_DIR/`mktemp commit.XXX`
+BRANCH_LOG1=$CUR_DIR/`mktemp branch_log.XXX`
+BRANCH_LOG2=$CUR_DIR/`mktemp branch_log.XXX`
+COMMENTS_BRANCH2=$CUR_DIR/`mktemp comments.XXX`
 
 function usage() 
 {
@@ -26,20 +29,27 @@ function get_git_log()
 {
 	cd $GIT_DIR
 	git checkout $BRANCH1
-	git log e2f712dc927e3b9a981ecd86a64d944d0b140322..HEAD --pretty=oneline > $CUR_DIR/$BRANCH_LOG1
+	git log e2f712dc927e3b9a981ecd86a64d944d0b140322..HEAD --pretty=oneline > $BRANCH_LOG1
 	git branch
 
 	git checkout $BRANCH2
-	git log e2f712dc927e3b9a981ecd86a64d944d0b140322..HEAD --pretty=oneline > $CUR_DIR/$BRANCH_LOG2
+	git log e2f712dc927e3b9a981ecd86a64d944d0b140322..HEAD --pretty=oneline > $BRANCH_LOG2
 	git branch
 }
 
+# First, the function below will create 2 files.
+# One with just the commit hashes and another with only the comment
+# header. Then it will run fgrep from each over the log and output the
+# difference to diff_commits-final.txt
+
 function search_commit()
 {
-	cut -f1 -d' ' $CUR_DIR/$BRANCH_LOG2 > $CUR_DIR/$COMMIT_BRANCH2
-#	while read commit; do
-#		fgrep "$commit" $CUR_DIR/$BRANCH1.log
-#	done< "$CUR_DIR/commit_$BRANCH1.log"
+	cut -f1 -d' ' $BRANCH_LOG2 > $COMMIT_BRANCH2
+	cut --fields=2- -d' ' $BRANCH_LOG2 > $COMMENTS_BRANCH2
+	fgrep -v -f $COMMIT_BRANCH2 $BRANCH_LOG1 > $CUR_DIR/diff_commits1.txt
+	fgrep -v -f $COMMENTS_BRANCH2 $CUR_DIR/diff_commits1.txt > $CUR_DIR/diff_commits-final.txt
+	fgrep -f $COMMENTS_BRANCH2 $CUR_DIR/diff_commits1.txt > $CUR_DIR/common_commits.txt
+	rm $CUR_DIR/diff_commits1.txt
 }
 
 if [ ! -d $1 ]; then
@@ -56,4 +66,4 @@ fi
 get_git_log
 search_commit
 cd $CUR_DIR
-#rm $COMMIT_BRANCH1 $COMMIT_BRANCH2 $BRANCH_LOG1 $BRANCH_LOG2
+rm $COMMIT_BRANCH1 $COMMIT_BRANCH2 $BRANCH_LOG1 $BRANCH_LOG2 $COMMENTS_BRANCH2
